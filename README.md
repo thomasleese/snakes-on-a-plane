6091df9ba6f0916d130218b976f0b3d30ba00366
Tom Leese <inbox@tomleese.me.uk>
2015-05-14 15:58:48 +0100

    Organise as a Python module

--- README.md
+++
@@ -1,2 +0,0 @@
-# snakes-on-a-plane
-A Python module for interfacing with X-Plane through the UDP protocol.
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+from xplane import __version__
+
+
+with open('README.rst') as fd:
+    long_description = fd.read()
+
+
+setup(
+    name='snakes-on-a-plane',
+    version=__version__,
+    description='A set of tools for interfacing with X-Plane 10.',
+    long_description=long_description,
+    author='Tom Leese',
+    author_email='inbox@tomleese.me.uk',
+    url='https://github.com/tomleese/pyxplane',
+    packages=['xplane'],
+    install_requires=['Pint'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ]
+)
